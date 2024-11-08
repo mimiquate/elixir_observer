49a1c25386cdcc180ed83bf6f8889c7ed076f3ab
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-11-08 14:06:09 -0300

    pagination

--- main.exs
+++ main.exs
@@ -3,11 +3,11 @@ Application.ensure_all_started([:inets, :ssl])
 defmodule Hexpm do
   @base_url "https://hex.pm/api"
 
-  def get do
+  def get_page(page) do
     :httpc.request(
       :get,
       {
-        ~c"#{@base_url}/packages",
+        ~c"#{@base_url}/packages?sort=downloads&page=#{page}",
         [{~c"user-agent", "httpc"}]
       },
       [
@@ -22,14 +22,19 @@ defmodule Hexpm do
   end
 end
 
-{
-  :ok,
+1..20
+|> Enum.each(fn page ->
   {
-    {_, 200, _},
-    _headers,
-    packages_data
-  }
-} =
-  Hexpm.get()
+    :ok,
+    {
+      {_, 200, _},
+      _headers,
+      packages_data
+    }
+  } =
+    Hexpm.get_page(page)
 
-File.write!("packages.json", packages_data)
+  File.write!("packages_data/packages-#{String.pad_leading("#{page}", 3, "0")}.json", packages_data)
+
+  Process.sleep(5_000)
+end)
