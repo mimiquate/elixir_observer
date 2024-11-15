7dea11598da5b682ea078a5f5cd9a7f16fe133d9
Gonzalo <456459+grzuy@users.noreply.github.com>
2024-11-15 13:33:04 -0300

    ci: set up

--- config/test.exs
+++ config/test.exs
@@ -6,8 +6,8 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :elixir_toolbox, ElixirToolbox.Repo,
-  username: "postgres",
-  password: "postgres",
+  username: "elixir_toolbox",
+  password: "elixir_toolbox",
   hostname: "localhost",
   database: "elixir_toolbox_test#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
