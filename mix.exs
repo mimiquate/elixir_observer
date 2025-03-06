06bd01d80673cfa1b126de45f4e8be04fe523504
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-03-06 14:02:15 -0300

    build(fly): don't include release artifacts for windows

--- mix.exs
+++ mix.exs
@@ -9,7 +9,12 @@ defmodule Toolbox.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      releases: [
+        toolbox: [
+          include_executables_for: [:unix]
+        ]
+      ]
     ]
   end
 
--- rel/overlays/bin/migrate.bat
+++
@@ -1,1 +0,0 @@
-call "%~dp0\toolbox" eval Toolbox.Release.migrate
