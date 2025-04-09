aeba666d723226c41b32783c5eeeaee15f72de6e
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-04-09 11:10:19 -0300

    feat: admin live dashboard

--- lib/toolbox_web/router.ex
+++ lib/toolbox_web/router.ex
@@ -15,6 +15,11 @@ defmodule ToolboxWeb.Router do
     plug :accepts, ["json"]
   end
 
+  pipeline :admin do
+    plug :put_layout, html: {JustPollsWeb.Layouts, :admin}
+    plug :admin_basic_auth
+  end
+
   scope "/", ToolboxWeb do
     pipe_through :browser
 
@@ -23,28 +28,18 @@ defmodule ToolboxWeb.Router do
     get "/packages/:name", PackageController, :show
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", ToolboxWeb do
-  #   pipe_through :api
-  # end
+  scope "/admin", ToolboxWeb do
+    pipe_through [:browser, :admin]
 
-  # Enable LiveDashboard in development
-  if Application.compile_env(:toolbox, :dev_routes) do
-    # If you want to use the LiveDashboard in production, you should put
-    # it behind authentication and allow only admins to access it.
-    # If your application does not have an admins-only section yet,
-    # you can use Plug.BasicAuth to set up some basic authentication
-    # as long as you are also using SSL (which you should anyway).
     import Phoenix.LiveDashboard.Router
-
-    scope "/dev" do
-      pipe_through :browser
-
-      live_dashboard "/dashboard", metrics: ToolboxWeb.Telemetry
-    end
+    live_dashboard "/dashboard", metrics: ToolboxWeb.Telemetry
   end
 
   defp basic_auth(conn, _opts) do
+    Plug.BasicAuth.basic_auth(conn, Application.fetch_env!(:toolbox, :basic_auth))
+  end
+
+  defp admin_basic_auth(conn, _opts) do
     Plug.BasicAuth.basic_auth(conn, Application.fetch_env!(:toolbox, :basic_auth))
   end
 end
