9a7b0dfe069b19d85bc46037730e6083e6d30da0
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-04-14 15:34:31 -0300

    feat: favicon

--- lib/toolbox_web.ex
+++ lib/toolbox_web.ex
@@ -17,7 +17,7 @@ defmodule ToolboxWeb do
   those modules here.
   """
 
-  def static_paths, do: ~w(assets fonts images favicon.ico robots.txt)
+  def static_paths, do: ~w(assets fonts images files robots.txt)
 
   def router do
     quote do
