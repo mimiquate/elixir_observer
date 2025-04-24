f21eacea90c340b90bf67dc6b8fb51559321e656
Juan Azambuja <juan@mimiquate.com>
2025-04-24 14:13:10 -0300

    support mobile correctly

--- lib/toolbox_web/components/stats_card.ex
+++ lib/toolbox_web/components/stats_card.ex
@@ -9,16 +9,16 @@ defmodule ToolboxWeb.Components.StatsCard do
 
   def stats_card(assigns) do
     ~H"""
-    <div class={"bg-zinc-100 dark:bg-surface px-4 py-6 #{@class}"}>
-      <div class="flex">
+    <div class={"bg-zinc-100 dark:bg-surface p-3 sm:px-4 sm:py-6 #{@class}"}>
+      <div class="flex items-center">
         <div class="p-1 bg-surface-alt w-fit rounded-md">
           <img src={@icon_path} />
         </div>
-        <h4 class="text-[18px] text-secondary-text ml-2">
+        <h4 class="text-[12px] sm:text-[18px] text-secondary-text ml-2">
           {@title}
         </h4>
       </div>
-      <div class="mt-4">
+      <div class="mt-2 sm:mt-4">
         {render_slot(@inner_block)}
       </div>
     </div>
