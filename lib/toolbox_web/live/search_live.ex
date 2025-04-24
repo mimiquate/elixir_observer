7d61ba6d2a236bb29e782fd8fad7a0b10827de8b
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2025-04-24 14:15:04 -0300

    feat: improve UX of search results

--- lib/toolbox_web/live/search_live.ex
+++ lib/toolbox_web/live/search_live.ex
@@ -1,14 +1,22 @@
 defmodule ToolboxWeb.SearchLive do
   use ToolboxWeb, :live_view
+  alias Toolbox.Packages
 
   def mount(%{"term" => term}, _session, socket) do
+    packages =
+      Packages.search(term)
+      |> Enum.map(fn package ->
+        # Fix N+1 Query
+        {package, Packages.last_hexpm_snapshot(package).data}
+      end)
+
     {
       :ok,
       assign(
         socket,
         term: term,
         page_title: "\"#{term}\"",
-        packages: Toolbox.Packages.search(term)
+        packages: packages
       )
     }
   end
