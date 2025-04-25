5030bb27fe2dfbc1f4ea6da4ebf37b0c4e70878c
Juan Manuel Azambuja <juan@mimiquate.com>
2025-04-25 09:40:29 -0300

    feat: display package resource links as designed (#42)
    * feat: display package links as designed
    
    * fix(package-show): decrease padding of header in desktop

---
+++ lib/toolbox_web/components/package_link.ex
@@ -0,0 +1,18 @@
+defmodule ToolboxWeb.Components.PackageLink do
+  use Phoenix.Component
+  use ToolboxWeb, :verified_routes
+
+  attr :href, :string, required: true
+  attr :icon_path, :string, required: true
+  attr :text, :string, required: true
+
+  def package_link(assigns) do
+    ~H"""
+    <.link class="flex items-center text-[14px] py-2 sm:px-4" href={@href} target="_blank">
+      <img src={@icon_path} class="mr-2 sm:w-4" />
+      <span class="sm:mt-0">{@text}</span>
+      <img src={~p"/images/right-chevron-icon.svg"} class="mr-2 ml-auto" />
+    </.link>
+    """
+  end
+end
