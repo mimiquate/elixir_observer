40977dd6bc6d4db53465497cd873d222bd7f6377
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2025-05-02 15:21:06 -0300

    chore: Add Task for ingesting hexpm packages by name

--- lib/toolbox/tasks/hexpm.ex
+++ lib/toolbox/tasks/hexpm.ex
@@ -15,23 +15,46 @@ defmodule Toolbox.Tasks.Hexpm do
       packages_data
       |> Jason.decode!()
       |> Enum.each(fn package_data ->
-        name = package_data["name"]
-
-        package =
-          case Toolbox.Packages.get_package_by_name(name) do
-            %Toolbox.Package{} = p ->
-              p
-
-            _ ->
-              {:ok, p} = Toolbox.Packages.create_package(%{name: name})
-              p
-          end
-
-        {:ok, _hexpm_snaphost} =
-          Toolbox.Packages.create_hexpm_snapshot(%{package_id: package.id, data: package_data})
+        create_hexpm_snapshot(package_data)
       end)
 
       Process.sleep(:timer.seconds(1))
     end)
+  end
+
+  def run(names) when is_list(names) do
+    names |> Enum.each(&run/1)
+  end
+
+  def run(name) when is_binary(name) do
+    {
+      :ok,
+      {
+        {_, 200, _},
+        _headers,
+        package_data
+      }
+    } = Toolbox.Hexpm.get_package(name)
+
+    package_data
+    |> Jason.decode!()
+    |> create_hexpm_snapshot()
+  end
+
+  defp create_hexpm_snapshot(package_data) do
+    name = package_data["name"]
+
+    package =
+      case Toolbox.Packages.get_package_by_name(name) do
+        %Toolbox.Package{} = p ->
+          p
+
+        _ ->
+          {:ok, p} = Toolbox.Packages.create_package(%{name: name})
+          p
+      end
+
+    {:ok, _hexpm_snaphost} =
+      Toolbox.Packages.create_hexpm_snapshot(%{package_id: package.id, data: package_data})
   end
 end
