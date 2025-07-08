416bd72e97f6bfd8ac70ab771c9b998b9c852377
Juan Azambuja <juan@mimiquate.com>
2025-07-08 11:59:09 -0300

    feat(inspect-source): show inspect icon properly

--- lib/toolbox_web/components/icons/inspect_icon.ex
+++ lib/toolbox_web/components/icons/inspect_icon.ex
@@ -1,28 +1,24 @@
 defmodule ToolboxWeb.Components.Icons.InspectIcon do
   use Phoenix.Component
 
-  attr :id, :string, default: nil
   attr :class, :string, default: nil
 
   def inspect_icon(assigns) do
     ~H"""
     <svg
-      id={@id}
-      class={@class}
-      width="32"
-      height="32"
-      viewBox="0 0 32 32"
+      viewBox="0 0 20 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
+      class={"stroke-accent dark:stroke-secondary-text fill-accent dark:fill-secondary-text #{@class}"}
     >
-      <rect width="32" height="32" fill="#FCFDFD" />
-      <g>
-        <rect width="19" height="19" fill="white" transform="translate(6.5 6.5)" />
-        <path
-          d="M12.334 22L6.83398 16.5L12.334 11L13.6402 12.3063L9.42357 16.5229L13.6173 20.7167L12.334 22ZM19.6673 22L18.3611 20.6938L22.5777 16.4771L18.384 12.2833L19.6673 11L25.1673 16.5L19.6673 22Z"
-          fill="#8956B7"
-        />
+      <g clip-path="url(#clip0_2269_5011)">
+        <path d="M6.3335 16L0.833496 10.5L6.3335 5L7.63975 6.30625L3.42308 10.5229L7.61683 14.7167L6.3335 16ZM13.6668 16L12.3606 14.6938L16.5772 10.4771L12.3835 6.28333L13.6668 5L19.1668 10.5L13.6668 16Z" />
       </g>
+      <defs>
+        <clipPath id="clip0_2269_5011">
+          <rect width="19" height="19" transform="translate(0.5 0.5)" />
+        </clipPath>
+      </defs>
     </svg>
     """
   end
