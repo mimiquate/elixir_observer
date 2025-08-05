985364f005e33bca288847cab98608dfe8aec480
Emiliano Silva <emisilvacab@gmail.com>
2025-08-04 21:26:25 -0300

    fix: assign home page title

--- lib/toolbox_web/live/home_live.ex
+++ lib/toolbox_web/live/home_live.ex
@@ -2,7 +2,7 @@ defmodule ToolboxWeb.HomeLive do
   use ToolboxWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket, layout: {ToolboxWeb.Layouts, :home}}
+    {:ok, assign(socket, page_title: "Home"), layout: {ToolboxWeb.Layouts, :home}}
   end
 
   def handle_info({:hide_dropdown, component_id}, socket) do
