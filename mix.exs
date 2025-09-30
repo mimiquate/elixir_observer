144d7f6d227990348bf0df4d5996e1268437e233
Gonzalo <456459+grzuy@users.noreply.github.com>
2025-09-30 10:33:41 -0300

    fixes warning

--- mix.exs
+++ mix.exs
@@ -8,6 +8,7 @@ defmodule Toolbox.MixProject do
       elixir: "~> 1.18.3",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix_live_view] ++ Mix.compilers(),
+      listeners: [Phoenix.CodeReloader],
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps(),
