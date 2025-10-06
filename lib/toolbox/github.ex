3688a8d6bca11cbe666d3424ed2940913be7bd5e
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2025-10-06 15:19:25 -0300

    chore: Use Req instead of httpc for Github requests

--- lib/toolbox/github.ex
+++ lib/toolbox/github.ex
@@ -17,93 +17,65 @@ defmodule Toolbox.Github do
       |> Calendar.strftime("%x")
 
     query = """
-      openIssueCount: search(type: ISSUE, query: "type:issue repo:#{owner}/#{repository_name} is:open created:>=#{year_ago}") {
-        issueCount
-      }
-      closedIssueCount: search(type: ISSUE, query: "type:issue repo:#{owner}/#{repository_name} is:closed created:>=#{year_ago}") {
-        issueCount
-      }
-      openPRCount: search(type: ISSUE, query: "type:pr repo:#{owner}/#{repository_name} is:open created:>=#{year_ago}") {
-        issueCount
-      }
-      mergedPRCount: search(type: ISSUE, query: "type:pr repo:#{owner}/#{repository_name} is:merged created:>=#{year_ago}") {
-        issueCount
-      }
-      repository(owner: \"#{owner}\", name: \"#{repository_name}\") {
-        pullRequests(last: 5, states: [MERGED]) {
-          nodes {
-            createdAt
-            mergedAt
-            permalink
-            mergedBy {
-              login
-              avatarUrl(size: 24)
-            }
-            title
-          }
+      query {
+        openIssueCount: search(type: ISSUE, query: "type:issue repo:#{owner}/#{repository_name} is:open created:>=#{year_ago}") {
+          issueCount
         }
-
-        latestTag: refs(refPrefix: "refs/tags/", first: 1, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {
-          nodes {
-            name
-            compare(headRef: "HEAD") {
-              aheadBy
+        closedIssueCount: search(type: ISSUE, query: "type:issue repo:#{owner}/#{repository_name} is:closed created:>=#{year_ago}") {
+          issueCount
+        }
+        openPRCount: search(type: ISSUE, query: "type:pr repo:#{owner}/#{repository_name} is:open created:>=#{year_ago}") {
+          issueCount
+        }
+        mergedPRCount: search(type: ISSUE, query: "type:pr repo:#{owner}/#{repository_name} is:merged created:>=#{year_ago}") {
+          issueCount
+        }
+        repository(owner: \"#{owner}\", name: \"#{repository_name}\") {
+          pullRequests(last: 5, states: [MERGED]) {
+            nodes {
+              createdAt
+              mergedAt
+              permalink
+              mergedBy {
+                login
+                avatarUrl(size: 24)
+              }
+              title
             }
           }
-        }
 
-        changelog: object(expression: "HEAD:CHANGELOG.md") {
-          oid
+          latestTag: refs(refPrefix: "refs/tags/", first: 1, orderBy: {field: TAG_COMMIT_DATE, direction: DESC}) {
+            nodes {
+              name
+              compare(headRef: "HEAD") {
+                aheadBy
+              }
+            }
+          }
+
+          changelog: object(expression: "HEAD:CHANGELOG.md") {
+            oid
+          }
         }
       }
     """
 
-    :httpc.request(
-      :post,
-      {
-        ~c"#{base_url()}/graphql",
-        [
-          {~c"authorization", "bearer #{authorization_token()}"},
-          {~c"user-agent", "elixir client"}
-        ],
-        ~c"application/json",
-        JSON.encode!(%{"query" => "query { #{query} }"})
-      },
-      [
-        ssl: [
-          verify: :verify_peer,
-          cacerts: :public_key.cacerts_get(),
-          # Support wildcard certificates
-          customize_hostname_check: [
-            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-          ]
-        ]
+    Req.post(
+      url: "#{base_url()}/graphql",
+      headers: [
+        {"authorization", "Bearer #{authorization_token()}"},
+        {"user-agent", "toolbox"}
       ],
-      []
+      json: %{"query" => query}
     )
   end
 
   defp get(path) do
-    :httpc.request(
-      :get,
-      {
-        ~c"#{base_url()}/#{path}",
-        [
-          {~c"authorization", "Bearer #{authorization_token()}"},
-          {~c"user-agent", "elixir client"}
-        ]
-      },
-      [
-        ssl: [
-          verify: :verify_peer,
-          cacerts: :public_key.cacerts_get(),
-          # Support wildcard certificates
-          customize_hostname_check: [
-            match_fun: :public_key.pkix_verify_hostname_match_fun(:https)
-          ]
-        ]
-      ],
-      []
+    Req.get("#{base_url()}/#{path}",
+      headers: [
+        {"authorization", "Bearer #{authorization_token()}"},
+        {"user-agent", "toolbox"}
+      ]
     )
   end
 
--- lib/toolbox/tasks/github.ex
+++ lib/toolbox/tasks/github.ex
@@ -8,12 +8,9 @@ defmodule Toolbox.Tasks.GitHub do
     case Github.parse_link(github_link) do
       %{"owner" => owner, "repo" => repository_name} ->
         case Github.get_repo(owner, repository_name) do
-          {:ok, {{_, 200, _}, _, repository_data}} ->
-            {:ok, {{_, 200, _}, _h, activity_data}} =
+          {:ok, %{status: 200, body: repository_data}} ->
+            {:ok, %{status: 200, body: activity_data}} =
               Github.get_activity_and_changelog(owner, repository_name)
-
-            repository_data = Jason.decode!(repository_data)
-            activity_data = Jason.decode!(activity_data)
 
             changelog = get_in(activity_data, ["data", "repository", "changelog"])
 
@@ -29,7 +26,7 @@ defmodule Toolbox.Tasks.GitHub do
               data: repository_data
             })
 
-          {:ok, {{_, 404, _}, _, _}} ->
+          {:ok, %{status: 404}} ->
             Logger.warning("GITHUB REPO for #{github_link} NOT FOUND")
             Packages.delete_github_snapshots(package)
             nil
