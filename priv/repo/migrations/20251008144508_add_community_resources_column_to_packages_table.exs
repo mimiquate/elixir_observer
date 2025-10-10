af93a17189d4df962b171737b5d5f11a34044809
Santiago Ferreira <san650@gmail.com>
2025-10-10 13:52:18 -0300

    fix(community) remove unneeded migration

--- priv/repo/migrations/20251008144508_add_community_resources_column_to_packages_table.exs
+++
@@ -1,9 +0,0 @@
-defmodule Toolbox.Repo.Migrations.AddCommunityResourcesColumnToPackagesTable do
-  use Ecto.Migration
-
-  def change do
-    alter table("packages") do
-      add :community_resources, :map
-    end
-  end
-end
