8072129540d99e6a6838c789f6599e659d88149d
Marcelo Dominguez <952074+marpo60@users.noreply.github.com>
2025-10-30 15:53:50 -0300

    chore(home-test): Simplify test
    We are not behind basic auth anymore

--- test/toolbox_web/features/home_test.exs
+++ test/toolbox_web/features/home_test.exs
@@ -3,11 +3,8 @@ defmodule ToolboxWeb.Features.HomeTest do
   use Wallaby.Feature
 
   feature "home page", %{session: session} do
-    # https://github.com/elixir-wallaby/wallaby/issues/331#issuecomment-1873618554
-    %URI{scheme: scheme, host: host, port: port} = ToolboxWeb.Endpoint.struct_url()
-
     session
-    |> visit("#{scheme}://admin:secret@#{host}:#{port}/")
+    |> visit("/")
     |> fill_in(Query.text_field("Find packages"), with: "bandit")
   end
 end
